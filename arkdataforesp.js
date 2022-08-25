6833df25a1eaa8052a1400b334fa694f8f4efc54
HappyOx6032 <111933419+HappyOx6032@users.noreply.github.com>
2022-08-25 23:51:33 +0800

    Create arkdataforesp.js

---
+++ arkdataforesp.js
@@ -0,0 +1,26 @@
+//Working for ARK 2.0.26
+
+//ARK Core Data
+
+//GWorld - 0x1a8
+//LocalPlayers - 0x38
+//localPlayerController - 0x0
+//mySelf - 0x30
+
+//playerCameraManager - 0x660
+//myTeam - 0x29c
+//0xbd0 - 0x10
+//team - 0x29C
+
+//Location Addresses
+
+//Pitch - 0xc
+//Yaw - 0xc
+//Roll - 0xc
+//PovAddr - 0x28
+
+//rootComponent - 0x2c8
+//World Postitions
+//X - 0x290
+//Y - 0x290
+//Z - 0x290
